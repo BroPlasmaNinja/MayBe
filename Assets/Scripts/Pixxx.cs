a3ac6ad059bdb055a1746acdd6027db0f8809c9d
BroPlasmaNinja <HeadMenlvl100@yandex.ru>
2021-03-22 17:28:53 +0400

    HELP ME!

---
+++ Assets/Scripts/Pixxx.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+public class Pixxx : MonoBehaviour
+{
+    [Range(1,256)]public float Scale = 2;
+    private float gg;
+
+    private Camera cameraComponent;
+    private RenderTexture texture;
+
+    private void Start()
+    {
+        CreateTexture();
+        gg = Scale;
+    }
+
+    private void CreateTexture()
+    {
+        int width = Mathf.RoundToInt(Screen.width / Scale);
+        int height = Mathf.RoundToInt(Screen.height / Scale);
+        texture = new RenderTexture(width, height, 24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
+        texture.antiAliasing = 1;
+
+        cameraComponent = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (gg==Scale) return;
+        CreateTexture();
+        gg = Scale;
+    }
+
+    private void OnPreRender()
+    {
+        cameraComponent.targetTexture = texture;
+    }
+
+    private void OnPostRender()
+    {
+        cameraComponent.targetTexture = null;
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        src.filterMode = FilterMode.Point;
+
+        Graphics.Blit(src, dest);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region SystemSettings
+    private float angleX;
+    private float angleY;
+    private CharacterController controller;
+    #endregion
+    #region HackerCanUse)
+    [SerializeField] private float speed;
+    [SerializeField] [Range(0,90)] float MaxView;
+    #endregion
+    #region PlayerCanUse
+    [SerializeField] private float sencivity;
+    #endregion
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        #region Rotate
+        var mouseX = Input.GetAxis("Mouse X") * sencivity;
+        var mouseY = Input.GetAxis("Mouse Y") * sencivity;
+
+        angleX -= mouseY;
+        angleY += mouseX;
+
+        angleX = Mathf.Clamp(angleX, -MaxView, MaxView);
+
+        gameObject.transform.localRotation = Quaternion.Euler(0, angleY, 0);
+        transform.GetChild(0).localRotation = Quaternion.Euler(angleX, 0, 0);
+        #endregion
+        #region Move
+        float X = Input.GetAxis("Horizontal");
+        float Z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * X + transform.forward * Z;
+        if (!Input.GetKey(KeyCode.LeftShift))
+            controller.Move(move * speed * Time.deltaTime);
+        else
+            controller.Move(move * speed * Time.deltaTime * 1.5f);
+        #endregion
+    }
+}
